fe62b46bb8ebeef3e69a5f1205a72cdf7690ad3e
Justin Hugon <jhugon@users.noreply.github.com>
2022-08-26 14:43:19 -0500

    feat: add montage images of all of the sprites
    To be able to look at them all in one place

--- makeSprites.sh
+++ makeSprites.sh
@@ -24,3 +24,20 @@ for i in spritesExt/nichol/*.png; do
   basename=${basename#"spritesExt/nichol/"}
     convert $i -shave 40x40 PNG32:sprites/$basename.png
 done
+
+######################################
+## Make montage of original sprites ##
+######################################
+
+tmpDir=/tmp/orbitalGame
+mkdir -p $tmpDir
+
+for color in white lightblue yellow red pink cyan magenta royalblue1 deepskyblue springgreen lime firebrick1; do
+  convert spritesOrig/Sprite*.png +append -channel RGB -fuzz 40% -fill $color -opaque black PNG32:$tmpDir/montageLand_$color.png
+done
+convert $tmpDir/montageLand_*.png -append PNG32:sprites/montageLand.png
+
+for color in white blue green yellow red pink; do
+  convert spritesOrig/!(Sprite*).png +append -channel RGB -fuzz 40% -fill $color -opaque white PNG32:$tmpDir/montageSpace_$color.png
+done
+convert $tmpDir/montageSpace_*.png -append PNG32:sprites/montageSpace.png
